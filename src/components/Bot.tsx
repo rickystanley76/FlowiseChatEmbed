acba13888e4b5847ec43e8cdae4f064af4e1ac3a
Ricky D'Cruze <rickystanley76@gmail.com>
2024-04-28 19:49:53 +0200

    remove chaning

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -995,7 +995,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
               />
             )}
           </div>
-          <Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />
+        
         </div>
       </div>
       {sourcePopupOpen() && <Popup isOpen={sourcePopupOpen()} value={sourcePopupSrc()} onClose={() => setSourcePopupOpen(false)} />}
@@ -1009,3 +1009,5 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
 // const BottomSpacer = (props: BottomSpacerProps) => {
 //   return <div ref={props.ref} class="w-full h-32" />;
 // };
+
+//<Badge badgeBackgroundColor={props.badgeBackgroundColor} poweredByTextColor={props.poweredByTextColor} botContainer={botContainer} />
