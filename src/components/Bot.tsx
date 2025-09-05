3aede34044373a493dedeb69b52bd16766a4f6a5
Ricky D Cruze <rickystanley76@gmail.com>
2025-09-05 15:21:13 +0200

    remove branding v3

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -2001,12 +2001,7 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
                 />
               )}
             </div>
-            <Badge
-              footer={props.footer}
-              badgeBackgroundColor={props.badgeBackgroundColor}
-              poweredByTextColor={props.poweredByTextColor}
-              botContainer={botContainer}
-            />
+
           </div>
         </div>
       )}
